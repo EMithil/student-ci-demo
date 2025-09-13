b0cde431a6cee4250acd02135cd725c8a70bee46
EMithil <emmadimithil@gmail.com>
2025-09-13 20:52:08 +0530

    Updated message in Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -2,4 +2,5 @@ package hello;
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello from  Pipeline CI");
+		System.out.println("Hello from Poll SCM trigger");
 }}
