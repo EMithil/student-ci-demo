91785ef1ab18ef59c5fb1105ae7dea4f1bf3becf
E Mithil <147174890+EMithil@users.noreply.github.com>
2025-09-13 23:29:48 +0530

    Update Hello.java(added a new line)

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,4 +3,5 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello from  Pipeline CI");
 		System.out.println("Hello from Poll SCM trigger");
+		System.out.println("Updated to check webhooks working");
 }}
